a9c3fa9d23b1e16b4c88bbb44732db24c4c44248
Cahya Revanto <64825576+revan2159@users.noreply.github.com>
2023-08-07 08:09:57 +0700

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,102 @@
+window.addEventListener("resize", function() {
+    "use strict"; window.location.reload(); 
+});
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    // make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+
+        document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
+            
+            everyitem.addEventListener('mouseover', function(e){
+
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                     nextEl.classList.add('show');
+                }
+                
+            });
+            everyitem.addEventListener('mouseleave', function(e){
+                 let el_link = this.querySelector('a[data-bs-toggle]');
+                
+                if(el_link != null){
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                     nextEl.classList.remove('show');
+                }
+                
+
+            })
+        });
+
+    }
+    
+}); 
+
+$(function () {
+    $("img").on("contextmenu", function () {
+        alert("Right click disabled");
+        return false;
+    });
+
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+          return [...document.querySelectorAll(el)]
+        } else {
+          return document.querySelector(el)
+        }
+      }
+    //navbar scroll effect
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+
+    $('.navbar-search-open').on('click', function(){
+        $('.box').toggleClass('open');
+    });
+    $('.navbar-search-close').on('click', function(){
+        $('.box').removeClass('open');
+    });
+
+    let heroCarouselIndicators = select("#hero-carousel-indicators")
+    let heroCarouselItems = select('#heroCarousel .carousel-item', true)
+  
+    heroCarouselItems.forEach((item, index) => {
+      (index === 0) ?
+      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
+        heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
+    });
+
+
+    var swiper = new Swiper(".hero", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+        loop: true,
+        
+        effect: "fade",
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
+
+
+
+});
+
+
